6608f05aff3c7bae7b7ef7e20175366fef99b699
Tom60340 <83062505+Tom60340@users.noreply.github.com>
2023-09-06 11:55:15 +0200

    Revert "added package:model,service,repository + Product class + ProductRepos…"

--- src/main/java/com/heroku/mercadona/controller/MainController.java
+++ src/main/java/com/heroku/mercadona/controller/MainController.java
@@ -1,12 +1,7 @@
 
 package com.heroku.mercadona.controller;
 
-import com.heroku.mercadona.model.Product;
-import com.heroku.mercadona.service.ProductService;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -16,6 +11,5 @@ public class MainController {
     public String showHomePage(){
         return "index";
     }
-    
     
 }
