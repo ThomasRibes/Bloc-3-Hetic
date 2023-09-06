4f7621309cef357b2c7bed8029d3ed7e185ae7d8
unknown <thomas.ribes.dev@outlook.fr>
2023-09-06 11:52:51 +0100

    removed useless import in Main controller

--- src/main/java/com/heroku/mercadona/controller/MainController.java
+++ src/main/java/com/heroku/mercadona/controller/MainController.java
@@ -1,12 +1,7 @@
 
 package com.heroku.mercadona.controller;
 
-import com.heroku.mercadona.model.Product;
-import com.heroku.mercadona.service.ProductService;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
