6057bf2a9de56581545d2f8a6e6249ecc7e995c9
unknown <thomas.ribes.dev@outlook.fr>
2023-09-11 16:51:45 +0100

    added redirect to addProduct + edited showAdminProducts

--- src/main/java/com/heroku/mercadona/controller/ProductController.java
+++ src/main/java/com/heroku/mercadona/controller/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class ProductController {
@@ -57,7 +58,7 @@ public class ProductController {
         }
         this.productRepository.save(product);
         System.out.println(product);
-        return "showProducts";    
+        return "redirect:/catalog";    
     }
             
 
