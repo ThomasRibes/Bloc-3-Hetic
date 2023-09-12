440218d0183b4b7f48984fcd5aadc50f2c2fb7d0
unknown <thomas.ribes.dev@outlook.fr>
2023-09-12 08:58:55 +0100

    added features add discount & update & delete product buttons to html admin products list + updateProduct.html + methods in ProductController

--- src/main/java/com/heroku/mercadona/controller/ProductController.java
+++ src/main/java/com/heroku/mercadona/controller/ProductController.java
@@ -10,7 +10,9 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -47,7 +49,6 @@ public class ProductController {
         List<Category> listCategories = categoryService.getAllCategories();
         model.addAttribute("product", product);
         model.addAttribute("listCategories", listCategories);
-        System.out.println(listCategories);
         return "createProduct";
     }
     
@@ -58,8 +59,36 @@ public class ProductController {
         }
         this.productRepository.save(product);
         System.out.println(product);
-        return "redirect:/catalog";    
+        return "redirect:/admin";    
     }
-            
+    
+    @GetMapping("/admin/product/edit/{id}")
+    public String updateProductForm(@PathVariable("id") Integer id, Model model){
+        List<Category> listCategories = categoryService.getAllCategories();
+        model.addAttribute("listCategories", listCategories);
+        Product product = this.productRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("Invalid product id : " + id));
+        model.addAttribute("product",product);
+        return "updateProduct";    
+    }
+    
+    @PostMapping("/admin/product/update/{id}")
+    public String updateProduct(@PathVariable("id") Integer id, @Valid Product product, BindingResult result, Model model){
+        if (result.hasErrors()){
+            product.setId(id);
+            return "updateProduct";
+        }
+        productRepository.save(product);
+        return "redirect:/admin";
+    }
+    
+    @GetMapping("admin/product/delete/{id}")
+    public String deleteProduct(@PathVariable("id") Integer id, Model model){
+        Product product = this.productRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("Invalid product id : " + id));
+        this.productRepository.delete(product);
+        return "redirect:/admin";
+    }
+    
 
 }
