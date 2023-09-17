535099f764cb2dba7fb667909ae533a06fbf52dc
unknown <thomas.ribes.dev@outlook.fr>
2023-09-17 10:48:50 +0100

    edited CategoryRepositorytests.jave to use CategoryService and @Springboot and Junit5 Assertions

--- src/test/java/com/heroku/mercadona/CategoryRepositoryTests.java
+++ src/test/java/com/heroku/mercadona/CategoryRepositoryTests.java
@@ -1,31 +1,28 @@
 package com.heroku.mercadona;
 
 import com.heroku.mercadona.model.Category;
-import com.heroku.mercadona.repository.CategoryRepository;
-import org.assertj.core.api.Assertions;
+import com.heroku.mercadona.service.CategoryService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.Rollback;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Rollback(false)
+@SpringBootTest
 public class CategoryRepositoryTests {
 
     @Autowired
-    private CategoryRepository categoryTestRepository;
+    private CategoryService categoryService;
 
     @Test
     public void integrationTestListAllCategories() {
-        Iterable<Category> categories = categoryTestRepository.findAll();
+        //Arrange
+        //Act
+        Iterable<Category> categories = categoryService.getAllCategories();
 
-        Assertions.assertThat(categories).hasSizeGreaterThan(0);
+        long size = categories.spliterator().getExactSizeIfKnown();
 
-        for (Category category : categories) {
-            System.out.println(category);
-        }
+        //Assert
+        Assertions.assertTrue(size > 0, "Category list exists");
     }
 
 }
