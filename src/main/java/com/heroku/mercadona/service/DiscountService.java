1000036538a4b4c30793bf1866e491918cdea648
unknown <thomas.ribes.dev@outlook.fr>
2023-09-22 14:46:18 +0100

    added getAllDiscounts() in DiscountService and DiscountServiceImpl

--- src/main/java/com/heroku/mercadona/service/DiscountService.java
+++ src/main/java/com/heroku/mercadona/service/DiscountService.java
@@ -1,10 +1,12 @@
 package com.heroku.mercadona.service;
 
 import com.heroku.mercadona.model.Discount;
+import java.util.List;
 
 public interface DiscountService {
 
     void saveDiscount(Discount discount);
     void deleteDiscountById(Integer id);
     Discount getDiscountById(Integer id);
+    List<Discount> getAllDiscounts();
 }
--- src/main/java/com/heroku/mercadona/service/impl/DiscountServiceImpl.java
+++ src/main/java/com/heroku/mercadona/service/impl/DiscountServiceImpl.java
@@ -3,6 +3,7 @@ package com.heroku.mercadona.service.impl;
 import com.heroku.mercadona.model.Discount;
 import com.heroku.mercadona.repository.DiscountRepository;
 import com.heroku.mercadona.service.DiscountService;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
 
@@ -35,6 +36,11 @@ public class DiscountServiceImpl implements DiscountService {
             throw new RuntimeException("Discount not found for id : " + id);
         }
         return discount;
+    }
+    
+    @Override
+    public List<Discount> getAllDiscounts(){
+         return (List<Discount>) discountRepository.findAll();
     }
 
 }
