f7ed2be2d01b9eb8ed89b9de716ca2f31f4df2cb
unknown <thomas.ribes.dev@outlook.fr>
2023-09-25 09:08:46 +0100

    added actions.js to disable past date in discount form, affect only front

---
+++ src/main/resources/static/js/actions.js
@@ -0,0 +1,5 @@
+// for input date can't be a past date
+let today = new Date().toISOString().split('T')[0];
+document.getElementsByName("startDate")[0].setAttribute('min', today);
+document.getElementsByName("endDate")[0].setAttribute('min', today);
+
