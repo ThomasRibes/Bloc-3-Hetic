027c41b0908d5ccb1476e1ad2219e02dead75e56
unknown <thomas.ribes.dev@outlook.fr>
2023-10-03 22:38:05 +0100

    cleaned showProducts.html

--- src/main/resources/static/js/filters.js
+++ src/main/resources/static/js/filters.js
@@ -1,9 +1,10 @@
 
 async function getCatalog() {
-
     const response = await axios.get('/catalog/filter', {
         params: {categoryId: $('#category option:selected').val()}})
             .then(function (html) {
+//                add check if data is empty
+//                add product title do id productTitle
                 document.getElementById("ourFilteredProducts").innerHTML = html.data;
             })
             .catch(function (error) {
